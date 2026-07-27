744ede05bdd2525475ab72af16e580f9704c3df5
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2026-07-28 02:35:34 +0530

    Fixed Bugs

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,16 +1,17 @@
-const jwt = require('jsonwebtoken')
+import jwt from 'jsonwebtoken';
 import { configDotenv } from 'dotenv';
 import User from '../model/user.model.js';
 import bcrypt from 'bcrypt'
+import express from 'express'
 
-const generateToken = async ( userId, email ) => {
+const generateToken = ( userId, email ) => { // no async needed, don't use async with the generate Token
 
     const token = jwt.sign(
         {
             id : userId,
             email : email
         }, 
-        process.env.SECRET_KEY, 
+        "secretKey", 
         {
             expiresIn: '1h' 
     });
@@ -21,10 +22,10 @@ const generateToken = async ( userId, email ) => {
 const signUp = async (req, res) => {
 
     try {
-        const {name, email, password} = req.body;
-    
-        const user = await User.findOne({email : email});
 
+        const {name, email, password} = req.body;
+        console.log(`name : ${name}, Email : ${email} , Password : ${password}`)
+        const user = await User.findOne({email : email});
         if(user) {
             return res.status(404).json({
                 success: false,
@@ -32,11 +33,11 @@ const signUp = async (req, res) => {
             })
         }
 
+        console.log("Hased Password")
         const saltRound = 10;
         // Hashing a password
-
-        const hashedPassword = await bcrypt.hash(password, salt);
-
+        const hashedPassword = await bcrypt.hash(password, saltRound);
+        
         const newUser = await User.create({
             name,
             email,
@@ -52,41 +53,54 @@ const signUp = async (req, res) => {
         })
 
     } catch (error) {
-        
-
+        return res.status(500).json({
+            success: false,
+            message : "Interval Serval Errorrrrr"
+        })
     }
 }
 
-const login = async (res, req) => {
+const login = async (req, res) => {
 
-    const {email, password} = req.bobdy;
+    try {
+        
+        const {email, password} = req.body;
+        const user = await User.findOne({"email" : email});
 
-    const user = await findOne({email : email});
+        if(!user) {
+            return res.status(404).json({
+                success : false,
+                message : "Email or Password is wrong"
+            })
+        }
 
-    if(!user) {
-        return res.status(404).json({
+        console.log("Working")
+        const hashedPassword = user.password
+        const checkHashedPassword = bcrypt.compare(password, hashedPassword);
+
+        if(!checkHashedPassword) {
+            return res.status(404).json({
+                success : false,
+                message : "Email or password is wrong"
+            })
+        }
+
+        const token = generateToken(user._id, email);
+        if(!token ) {
+            console.log("Failed to generate token")
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Successfully login",
+            token : token
+        })
+
+    } catch (error) {
+        return res.status(500).json({
             success : false,
-            message : "Email or Password is wrong"
+            message : "Found some Error in login"
         })
     }
-
-    const checkHashedPassword = bcrypt.compare(password, hashedPassword);
-
-    if(!checkHashedPassword) {
-        return res.status(404).json({
-            success : false,
-            message : "Email or password is wrong"
-        })
-    }
-
-    const token = generateToken(user._id, email);
-
-    return res.status(200).json({
-        success: true,
-        message: "Successfully login",
-        token : token
-    })
-    
 }
 
 export {
