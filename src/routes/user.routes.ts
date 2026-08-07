6488944415640105518a66838705822abd48942e
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2026-08-07 14:24:01 +0530

    Added Routes for authorization

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,18 +1,21 @@
 
 import express, {type Express, type Request, type Response} from 'express'
+import roleCheck from '../middlewares/role.middleware.ts'
+import verifyToken from '../middlewares/auth.middleware.ts'
 
 const router = express.Router()
 
 // Only admin can access this router
-router.get('/admin', (req : Request, res : Response) => {
+
+router.get('/admin', verifyToken, roleCheck("admin"), (req : Request, res : Response) => {
     res.json({
         message : "Welcome Admin"
     })
 })
 
 // Both admin and Driver can access this router
-
-router.get('/driver', (req : Request, res : Response) => {
+// Role check is a function so  I need to return it
+router.get('/driver', verifyToken, roleCheck("admin", "driver"), (req : Request, res : Response) => {
     res.json({
         message : "Welcome Driver"
     })
@@ -20,7 +23,7 @@ router.get('/driver', (req : Request, res : Response) => {
 
 // Admin and Rider can access this router
 
-router.get('/rider', (req : Request, res : Response) => {
+router.get('/rider', verifyToken, roleCheck("admin", "rider"), (req : Request, res : Response) => {
     res.json({
         message : "Welcome Rider"
     })
