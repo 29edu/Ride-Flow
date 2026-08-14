72e4de9021bbc510690d1d72bd01f0081421d2ea
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2026-08-14 19:02:46 +0530

    Added Reset Password

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -7,13 +7,16 @@ import { Types } from 'mongoose';
 import { activeSessionStorage } from '../services/session.service.ts';
 import { randomUUID } from 'node:crypto';
 
-const generateToken = ( userId : Types.ObjectId , email : String ) => { // no async needed, don't use async with the generate Token
+const generateToken = ( userId : Types.ObjectId , email : String, role : String ) => { // no async needed, don't use async with the generate Token
 
     const token = jwt.sign(
         {
             id : userId,
-            email : email
+            email : email,
+            role : role
+
         }, 
+
         "secretKey", 
         {
             expiresIn: '1h' 
@@ -25,10 +28,10 @@ const generateToken = ( userId : Types.ObjectId , email : String ) => { // no as
 const signUp = async (req : Request, res : Response) => {
 
     try {
-
+        
         const {name, email, password, role} = req.body;
 
-        if(!name || !email || !password) {
+        if(!name || !email || !password || !role) {
             return res.status(400).json({
                 success : false,
                 message : "Invalid Data"
@@ -53,10 +56,11 @@ const signUp = async (req : Request, res : Response) => {
         const newUser = await User.create({
             name,
             email,
-            password : hashedPassword
+            password : hashedPassword,
+            role
         })
 
-        const token = generateToken(newUser._id, email);
+        const token = generateToken(newUser._id, email, role);
         const deviceId = randomUUID(); // This will be stored locally
 
         // const sessionId = await activeSessionStorage(email, deviceId, "Not Submitted" );
@@ -88,7 +92,7 @@ const login = async (req : Request, res : Response) => {
         if(!user) {
             return res.status(404).json({
                 success : false,
-                message : "Email or Password is wrong...."
+                message : "Email doesn't exist"
             })
         }
 
@@ -103,7 +107,7 @@ const login = async (req : Request, res : Response) => {
             })
         }
 
-        const token = generateToken(user._id, email);
+        const token = generateToken(user._id, email, user.role);
         if(!token ) {
             console.log("Failed to generate token")
         }
@@ -126,7 +130,52 @@ const login = async (req : Request, res : Response) => {
     }
 }
 
+const resetPassword = async ( req : Request, res : Response) => {
+
+    try {
+        
+        const {email, newpassword} = req.body;
+
+        const user = await User.findOne({email});
+
+        if(!user) {
+            return res.json({
+                success : false,
+                message : "user email doesn't exist"
+            })
+        }
+
+        const saltRound = await genSalt(10);
+        const hashedPassword = await hash(newpassword, saltRound);
+
+        const updatedUser = await User.findOneAndUpdate(
+            {email : email},
+            {
+                $set : {
+                    password: hashedPassword
+                }
+            }
+        )
+
+        return res.status(200).json({
+            success: true,
+            message: "Password updated successfully",
+            userId: updatedUser?._id
+        });
+
+    } catch (err) {
+        
+        if(err instanceof Error) {
+            console.log("Error in reset Password", err.message + " " + err.stack)
+        }
+        else {
+            console.log("Error in reset password", err.stack)
+        }
+    }
+}
+
 export {
     signUp,
-    login
+    login,
+    resetPassword
 }
