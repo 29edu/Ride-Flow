03f3c9015ac741f7de82c9e8700e05ea3ee7922e
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2026-09-11 15:56:07 +0530

    Updated location service to store h3cellId

--- src/services/location.service.ts
+++ src/services/location.service.ts
@@ -2,21 +2,26 @@
 import {type Request, type Response} from 'express'
 import { UUID } from 'mongodb';
 import { client } from '../config/redis.ts';
+import {h3} from 'h3-js'
 
 const locationStore = async (req : Request, res : Response) => {
 
-    const {driverId, latitude, longitude, timestamp, speed, } = req.body; // validation of the information will be done in the middleware
+    const {driverId, latitude, longitude, timestamp, speed} = req.body; // validation of the information will be done in the middleware
 
     try {
 
         const redisKey = `locationDriverId:${driverId}`;
+        const resolution = 8;
+
+        const h3cellId =  h3.latLngToCell(latitude, longitude, resolution);
 
         await client.hSet(redisKey, {
             'latitude' : latitude,
             'longitude' : longitude,
             'clientTimeStamp' : timestamp,
             'severTimeStamp' : Date.now().toString(),
-            'speed' : speed
+            'speed' : speed,
+            'h3cellId' : h3cellId
         })
 
         return res.status(201).json({
@@ -24,8 +29,13 @@ const locationStore = async (req : Request, res : Response) => {
             message : "Successfully stored Driver Location Details",
         })
 
-    } catch (error) {
+    } catch (error : unknown) {
         
+        if(error instanceof Error) {
+            console.log("Found Error in the location update : ", error.message );
+        } else {
+            console.log("Error in the location update : ", error)
+        }
     }
 }
 
